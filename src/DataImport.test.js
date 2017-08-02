c1db6ff50a8335fb7cd16eb31b976d01b125fe6a
GerwinBosch <gerwinbosch@chello.nl>
2017-08-02 13:48:24 +0200

    Merge GI-16 Replaced line ending form crlf to lf # Conflicts: #	src/App.js #	src/DataImport.js #	src/DownloadView.js

--- src/DataImport.test.js
+++ src/DataImport.test.js
@@ -2,26 +2,22 @@
  * Created by Gerwin Bosch on 3-7-2017.
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
 // import * as X from './DataImport';
 import App from './App';
 import DataImport from './DataImport';
 import ReactTestUtils from 'react-dom/test-utils'; // ES6
-import ShallowRenderer from 'react-test-renderer/shallow'; // ES6
-import { mount,shallow } from "enzyme";
+import { mount, shallow } from 'enzyme';
 
-describe("<DataImport/>", () => {
-    it('renders without crashing', () => {
-        const wrapper = shallow(<DataImport/>);
-    });
+describe('<DataImport/>', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<DataImport />);
+  });
 
-    it('props setup', () => {
-       const outer = mount(
-           <App
-           />);
-       outer.setState({state:2})
-        outer.find()
-    });
-
+  it('props setup', () => {
+    const outer = mount(
+      <App />);
+    outer.setState({ state: 2 });
+    outer.find();
+  });
 });
 
