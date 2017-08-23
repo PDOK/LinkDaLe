70ff123a7bcaddc9075fc7732d4c912f75ee00ee
StanRonzhin <StanRonzhin@gmail.com>
2017-08-23 12:44:59 +0200

    GI-03 | Move expand from image to the card

--- src/Tutorialised.js
+++ src/Tutorialised.js
@@ -4,22 +4,28 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
+import './Tutorialised.css'
 class Tutorialised extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            tut1:{
+                expanded:false
+            },
+            tut2:{
+                expanded:false
+            }
+        }
+    }
 
 render(){
     return (
-        <div>
-        <Card style={{width:'48%', float:"left", margin: '10px 10px 10px 10px'}} >
-            <CardHeader
-                actAsExpander={true}
-                showExpandableButton={true}
-            />
-
-            <CardMedia style={{width:'90%',  marginLeft:'auto', marginRight:'auto'}}>
+        <div className="TutorialWrapper">
+        <Card className="TutorialCard" expanded={this.state.tut1.expanded} onClick={() => this.setState({tut1:{expanded:!this.state.tut1.expanded}})}>
+            <CardMedia style={{width:'90%',  marginLeft:'auto', marginRight:'auto'}} >
                 <img  src={process.env.PUBLIC_URL + '/images/tut1.png'}  />
             </CardMedia>
-            <CardTitle title="Getting Started" subtitle="Learn Linked Data"  actAsExpander={true} />
+            <CardTitle title="Getting Started" subtitle="Learn Linked Data"/>
             <CardText expandable>
                 Linked Data is a method of publishing structured data so that it can be linked and queried.
                 With this tutorial you will learn the basics of the Linked Data technology and will generate your first Linked Data.
@@ -30,12 +36,7 @@ render(){
                 <FlatButton label="Exercise: My first Linked Data" />
             </CardActions>
         </Card>
-            <Card style={{width:'48%', float:"left", margin: '10px 10px 10px 10px'}} >
-                <CardHeader
-                    actAsExpander={true}
-                    showExpandableButton={true}
-                />
-
+            <Card className="TutorialCard" expanded={this.state.tut2.expanded} onClick={() => this.setState({tut2:{expanded:!this.state.tut2.expanded}})}>
                 <CardMedia style={{width:'90%',  marginLeft:'auto', marginRight:'auto'}}>
                     <img  src={process.env.PUBLIC_URL + '/images/bpil.png'}  />
                 </CardMedia>
@@ -51,6 +52,38 @@ render(){
                     <FlatButton label="Exercise 2" />
                 </CardActions>
             </Card>
+            <Card className="TutorialCard" expanded={this.state.tut1.expanded} onClick={() => this.setState({tut1:{expanded:!this.state.tut1.expanded}})}>
+                <CardMedia style={{width:'90%',  marginLeft:'auto', marginRight:'auto'}} >
+                    <img  src={process.env.PUBLIC_URL + '/images/tut1.png'}  />
+                </CardMedia>
+                <CardTitle title="Getting Started" subtitle="Learn Linked Data"/>
+                <CardText expandable>
+                    Linked Data is a method of publishing structured data so that it can be linked and queried.
+                    With this tutorial you will learn the basics of the Linked Data technology and will generate your first Linked Data.
+                </CardText >
+                <CardActions expandable>
+                    <FlatButton label="About the tool" />
+                    <FlatButton label="About Linked Data" />
+                    <FlatButton label="Exercise: My first Linked Data" />
+                </CardActions>
+            </Card>
+            <Card className="TutorialCard" expanded={this.state.tut2.expanded} onClick={() => this.setState({tut2:{expanded:!this.state.tut2.expanded}})}>
+                <CardMedia style={{width:'90%',  marginLeft:'auto', marginRight:'auto'}}>
+                    <img  src={process.env.PUBLIC_URL + '/images/bpil.png'}  />
+                </CardMedia>
+                <CardTitle title="Business Process Integration Lab" subtitle="Linked Data for Supply-chain integration"  actAsExpander={true} />
+                <CardText expandable>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText >
+                <CardActions expandable>
+                    <FlatButton label="Exercise 1: Unicorn Trade" />
+                    <FlatButton label="Exercise 2" />
+                </CardActions>
+            </Card>
+
         </div>
     )
 }
