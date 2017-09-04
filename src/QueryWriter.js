bc63fcc5c9b4a719d81dfa35f132661500d612c8
Gerwin Bosch <gerwinbosch@chello.nl>
2017-09-04 11:02:58 +0200

    CI 34 | Added a floating action button to the editor

--- src/QueryWriter.js
+++ src/QueryWriter.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import CodeMirror from 'react-codemirror';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Play from 'material-ui/svg-icons/av/play-arrow';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/sparql/sparql';
 import 'codemirror/theme/material.css';
@@ -28,6 +30,7 @@ class QueryWriter extends React.Component {
           name="sparql query editor"
           value={this.state.query}
         />
+        <FloatingActionButton style={{ right: '40px', top: '100px', position: 'absolute' }}><Play /></FloatingActionButton>
       </div>
     );
   }
